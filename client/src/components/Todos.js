da54b2228263b7ae6e462d439e0187ff46d8114a
ktan114 <ktaneight@gmail.com>
2019-11-07 23:31:36 -0500

    Set up table to display todos

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 const Todos = props => {
+  const data = props.todos;
+  const columns = [
+    {
+      Header: 'Name',
+      accessor: 'name',
+    },
+    {
+      Header: 'Due Date',
+      accessor: 'due_date',
+    },
+    {
+      Header: 'Type',
+      accessor: 'type',
+    },
+    {
+      Header: 'Notes',
+      accessor: 'notes',
+    },
+    {
+      id: 'is_finished',
+      Header: 'Completed',
+      accessor: data => <input type="checkbox" checked={data.is_finished} />,
+    },
+  ];
   return (
     <div>
-      {props.todos.map(todo => (
-        <div>{todo.name}</div>
-      ))}
+      <ReactTable
+        data={data}
+        resolveData={data => data.map(row => row)}
+        columns={columns}
+        defaultPageSize={5}
+      />
     </div>
   );
 };
