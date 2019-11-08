f895c24ce807251896b6d3b94a0c71059a936c25
ktan114 <ktaneight@gmail.com>
2019-11-07 23:33:42 -0500

    Have only one filter button

--- client/src/components/ChangeView.js
+++ client/src/components/ChangeView.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 const ChangeView = props => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', width: '50%'}}>
+    <div>
       <button onClick={props.onClick}>Filter Completed Todos</button>
-      <button onClick={() => props.sort('due_date')}>Sort By Due Date</button>
-      <button onClick={() => props.sort('name')}>Sort By Name</button>
-      <button onClick={() => props.sort('type')}>Sort By Type</button>
-      <button onClick={props.getTodos}>Original</button>
     </div>
   );
 };
--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,9 +1,5 @@
 const filteredTodos = todos => todos.filter(todo => !todo.is_finished);
 
-const sortTodos = (todos, field) =>
-  todos.sort((a, b) => (a[field] < b[field] ? -1 : 1));
-
 module.exports = {
   filteredTodos,
-  sortTodos,
 };
