ad35c4117f03464e8b714df44b607e145a25d504
ktan114 <ktaneight@gmail.com>
2019-11-07 19:17:56 -0500

    Added a sort method

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,5 +1,9 @@
 const filteredTodos = todos => todos.filter(todo => !todo.is_finished);
 
+const sortTodos = (todos, field) =>
+  todos.sort((a, b) => (a[field] < b[field] ? -1 : 1));
+
 module.exports = {
-    filteredTodos,
-}
+  filteredTodos,
+  sortTodos,
+};
