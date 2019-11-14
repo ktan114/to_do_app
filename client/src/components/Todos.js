26400933bfbd07824c520f951b0e335d7d0fda67
ktan114 <ktaneight@gmail.com>
2019-11-14 14:15:56 -0500

    Refactored Todos file

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -3,79 +3,12 @@ import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import moment from 'moment';
 
-import {
-  handleEdit,
-  handleDelete,
-  renderEditable,
-  editDate,
-} from '../utils/helper';
+import tableConfig from '../utils/tableConfig';
 
 const Todos = props => {
   const data = props.todos;
 
-  const columns = [
-    {
-      Header: 'Changes',
-      accessor: 'id',
-      Cell: cellInfo => {
-        return (
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <button
-              onClick={() => {
-                handleEdit(cellInfo.value, data, props.getTodos);
-              }}
-            >
-              Save
-            </button>
-            <button
-              onClick={() => {
-                handleDelete(cellInfo.value, props.getTodos);
-              }}
-              style={{ color: 'red' }}
-            >
-              Delete
-            </button>
-          </div>
-        );
-      },
-    },
-    {
-      Header: 'Name',
-      accessor: 'name',
-      Cell: cellInfo => renderEditable(cellInfo, data),
-    },
-    {
-      id: 'due_date',
-      Header: 'Due Date',
-      accessor: data => (
-        <span>{moment(data.due_date).format('MM/DD/YYYY')}</span>
-      ),
-      Cell: cellInfo => editDate(cellInfo, data),
-    },
-    {
-      Header: 'Type',
-      accessor: 'type',
-      Cell: cellInfo => renderEditable(cellInfo, data),
-    },
-    {
-      Header: 'Notes',
-      accessor: 'notes',
-      Cell: cellInfo => renderEditable(cellInfo, data),
-    },
-    {
-      id: 'is_finished',
-      Header: 'Completed',
-      accessor: data => (
-        <input
-          type="checkbox"
-          checked={data.is_finished}
-          onChange={e => {
-            data.is_finished = !data.is_finished;
-          }}
-        />
-      ),
-    },
-  ];
+  const columns = tableConfig(data, props.getTodos);
 
   return (
     <div>
---
+++ client/src/utils/tableConfig.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import moment from 'moment';
+
+import { handleEdit, handleDelete, renderEditable, editDate } from './helper';
+
+const tableConfig = (props, getTodos) => {
+  const columns = [
+    {
+      Header: 'Changes',
+      accessor: 'id',
+      Cell: cellInfo => {
+        return (
+          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <button
+              onClick={() => {
+                handleEdit(cellInfo.value, props.data, getTodos);
+              }}
+            >
+              Save
+            </button>
+            <button
+              onClick={() => {
+                handleDelete(cellInfo.value, getTodos);
+              }}
+              style={{ color: 'red' }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      },
+    },
+    {
+      Header: 'Name',
+      accessor: 'name',
+      Cell: cellInfo => renderEditable(cellInfo, props.data),
+    },
+    {
+      id: 'due_date',
+      Header: 'Due Date',
+      accessor: data => (
+        <span>{moment(data.due_date).format('MM/DD/YYYY')}</span>
+      ),
+      Cell: cellInfo => editDate(cellInfo, props.data),
+    },
+    {
+      Header: 'Type',
+      accessor: 'type',
+      Cell: cellInfo => renderEditable(cellInfo, props.data),
+    },
+    {
+      Header: 'Notes',
+      accessor: 'notes',
+      Cell: cellInfo => renderEditable(cellInfo, props.data),
+    },
+    {
+      id: 'is_finished',
+      Header: 'Completed',
+      accessor: data => (
+        <input
+          type="checkbox"
+          checked={data.is_finished}
+          onChange={e => {
+            data.is_finished = !data.is_finished;
+          }}
+        />
+      ),
+    },
+  ];
+
+  return columns;
+};
+
+export default tableConfig;
